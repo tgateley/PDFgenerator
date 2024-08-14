ad3c56fe82e21677f6f68511b9119c55c51951aa
Tiffany <tgateley22@gmail.com>
2024-08-13 22:10:20 -0500

    Day 24: First Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from fpdf import FPDF
+
+pdf = FPDF(orientation='P', unit="mm", format="A4")
+
+df = pd.read_csv("topics.csv", sep=",")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1)
+    pdf.line(10,20,200,20)
+
+pdf.output("output.pdf")
