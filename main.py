87089097af0c0d1c81e5815c45838929bbbaf06c
Tiffany <tgateley22@gmail.com>
2024-08-13 23:07:59 -0500

    Day 24:add lines to every page

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ df = pd.read_csv("topics.csv", sep=",")
 
 for index, row in df.iterrows():
     pdf.add_page()
+
+    # Sets the Header
     pdf.set_font(family="Times", size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1)
     pdf.line(10,20,200,20)
 
+    for y in range(20, 280, 10):
+        pdf.line(10, y, 200, y)
 # Sets the Footer
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
@@ -21,6 +25,8 @@ for index, row in df.iterrows():
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        for y in range(20, 280, 10):
+            pdf.line(10, y, 200, y)
     # Sets the Footer
         pdf.ln(275)
         pdf.set_font(family="Times", style="I", size=8)
